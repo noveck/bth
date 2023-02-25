d19c2043a2db2bdb76c79074d999090bc90cc784
Noveck J. Gowandan <ngowandan@gmail.com>
2023-02-25 11:55:05 -0400

    beta release
    - Fixed array so list of words only displayed once.
    - When all words are finished, congrats message displayed.
    - Game over text edited
    - Play again button implemented
    - html made responsive (to test)

--- alpha/js/main.js
+++ alpha/js/main.js
@@ -1,30 +1,28 @@
-// Working no-repeat wordlist, game has endpoint, game 
-
 window.addEventListener('load', init);
 
 //Load More Words
 function load_more_words()
 {
-  var xmlhttp;
-  if (window.XMLHttpRequest)
-  {
-    //  IE7+, Firefox, Chrome, Opera, Safari 
-    xmlhttp=new XMLHttpRequest();
-  }
-  else
-  {
-    // IE6, IE5 
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function()
-  {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-    {
-      words=xmlhttp.responseText.split("\n");
-    }
-  }
-  xmlhttp.open("GET","words.txt",true);
-  xmlhttp.send();
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		//  IE7+, Firefox, Chrome, Opera, Safari 
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// IE6, IE5 
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			words=xmlhttp.responseText.split("\n");
+		}
+	}
+	xmlhttp.open("GET","words.txt",true);
+	xmlhttp.send();
 }
 
 // Globals
@@ -41,7 +39,7 @@ const currentLevel = levels.medium;
 
 let time = currentLevel;
 let score = 0;
-let isPlaying = false;
+let isPlaying;
 
 // DOM Elements
 const wordInput = document.querySelector('#word-input');
@@ -55,24 +53,22 @@ const highscoreDisplay = document.querySelector('#highscore');
 var words = ['cybersafett'];
 
 // Initialize Game
-
-
 function init() {
   // Show number of seconds in UI
   seconds.innerHTML = currentLevel;
-  // Fetch more words
-  load_more_words();
   // Load word from array
   showWord(words);
   // Start matching on word input
   wordInput.addEventListener('input', startMatch);
   // Call countdown every second
   setInterval(countdown, 1000);
-  // Initialize isPlaying to true
-  isPlaying = true;
+  // Check game status
+  setInterval(checkStatus, 50);
+  // Fetch more words
+  setTimeout(load_more_words(),1000)
 }
 
-//start match
+// Start match
 function startMatch() {
   if (matchWords()) {
     isPlaying = true;
@@ -91,7 +87,7 @@ function startMatch() {
 
   // Prevent display of High Score: -1
   if (sessionStorage['highscore'] >= 0) {
-    highscoreDisplay.innerHTML = sessionStorage['highscore'];
+  highscoreDisplay.innerHTML = sessionStorage['highscore'];
   }
 
   // If score is -1, display 0
@@ -100,9 +96,6 @@ function startMatch() {
   } else {
     scoreDisplay.innerHTML = score;
   }
-
-  // Check game status
-  checkStatus();
 }
 
 // Match currentWord to wordInput
@@ -118,42 +111,10 @@ function matchWords() {
 
 // Pick & show random word
 function showWord(words) {
-  if (words.length === 0) {
-    // End the game if there are no more words
-    isPlaying = false;
-    message.innerHTML = 'No more words!';
-    return;
-  }
   // Generate random array index
   const randIndex = Math.floor(Math.random() * words.length);
   // Output random word
   currentWord.innerHTML = words[randIndex];
-  // Remove word from array
-  words.splice(randIndex, 1);
-}
-
-function checkStatus() {
-  if (words.length === 0 && !isPlaying) {
-    message.innerHTML = '🏆 Congrats! You stopped the hacker!! 🏆';
-    wordInput.removeEventListener('input', startMatch);
-    document.querySelector('#play-again-btn').style.display = 'block';
-  } else if (words.length === 1 && currentWord.innerHTML === words[0]) {
-    // Last word in the array is displayed, end the game after it is matched
-    message.innerHTML = '🏆 Congrats! You stopped the hacker!! 🏆';
-    isPlaying = false;
-    wordInput.removeEventListener('input', startMatch);
-    document.querySelector('#play-again-btn').style.display = 'block';
-  } else if (!isPlaying && time === 0) {
-    message.innerHTML = '☠ ☠ You got hacked. Game Over!!! ☠ ☠';
-    score = -1;
-    wordInput.removeEventListener('input', startMatch);
-    document.querySelector('#play-again-btn').style.display = 'block';
-  }
-  
-  // Add click event listener to the button
-  document.querySelector('#play-again-btn').addEventListener('click', function() {
-    location.reload();
-  });
 }
 
 // Countdown timer
@@ -168,8 +129,12 @@ function countdown() {
   }
   // Show time
   timeDisplay.innerHTML = time;
-
-  // Check game status - moved to startgame func
-  checkStatus();
 }
 
+// Check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = 'Game Over!!!';
+    score = -1;
+  }
+}
